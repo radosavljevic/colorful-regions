7b269def6d306f58af207066a9d9253841af22fd
Branislava Radosavljević <bradosavljevic@Branislavas-MacBook-Pro.local>
2018-07-23 19:12:40 +0200

    Render diffferent color for each region

--- src/reactors/colorRegions.ts
+++ src/reactors/colorRegions.ts
@@ -2,9 +2,12 @@ import { Model } from "..";
 import * as vscode from 'vscode';
 // import {} from 'vscode/src/editor/contrib/folding/indentRangeProvider';
 import * as babel from 'babel-eslint';
+import * as ColorScheme from 'color-scheme';
+import * as Color from 'color';
 // import { computeRanges } from 'vs/editor/contrib/folding/indentRangeProvider';
 
 const parse = babel.parse;
+const scheme = new ColorScheme;
 
 const colorRegions = (model: Model) => {
     
@@ -27,12 +30,27 @@ const colorRegions = (model: Model) => {
     const regionsEnd = comments
         .filter(comment => comment.value.indexOf('#endregion') !== -1)
         .map(comment => activeTextEditor.document.positionAt(comment.start - 1));
+
+    // console.log('schecem', scheme);
+    const colors = scheme
+        .from_hue(21)
+        .distance(0.05)
+        .scheme('tetrade')        
+        .variation('pastel');
     
-    regionsStart.forEach((region, i) => range.push(new vscode.Range(regionsStart[i], regionsEnd[i])));
+    regionsStart.forEach((region, i) => {        
+        const range = [new vscode.Range(regionsStart[i], regionsEnd[i])];
+        const cl = Color(`#${colors.colors()[i]}`).color;
+        const overviewRulerColor = `rgba(${cl[0]}, ${cl[1]}, ${cl[2]}, 0.39)`;
+        const decorationType = vscode.window.createTextEditorDecorationType({
+            overviewRulerColor
+        });
+        activeTextEditor.setDecorations(decorationType, range);
+    });
     
     
-    // console.log('pera', regionsStart, regionsEnd);
-    activeTextEditor.setDecorations(decorationType, range);
+    // console.log('pera', colors);
+    // activeTextEditor.setDecorations(decorationType, range);
 };
 
 export default colorRegions;
